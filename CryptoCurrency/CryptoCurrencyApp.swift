974bff5d2cd139f127ac64d0c934869049aa3b16
Sonu Kumar <sonu.patna0808@gmail.com>
2023-07-03 08:29:37 +0530

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Sonu Kumar on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
