d1a03d3d3d84826f458066e44bba27fb30460249
Sonu Kumar <sonu.patna0808@gmail.com>
2023-07-03 13:14:26 +0530

    Adding Top Mover for Home view
     * Designed Topmover view
     * Design TopMoverItemView.

---
+++ CryptoCurrency/Core/Home/Views/TopMoversItemView.swift
@@ -0,0 +1,43 @@
+//
+//  TopMoversItemView.swift
+//  CryptoCurrency
+//
+//  Created by Sonu Kumar on 03/07/23.
+//
+
+import SwiftUI
+
+struct TopMoversItemView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(systemName: "bitcoinsign.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+                .padding(.bottom, 8)
+            
+            HStack(spacing: 2){
+                Text("BTC")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                
+                Text("23,000.00")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            
+            Text("+1.06%")
+                .font(.title2)
+                .foregroundColor(.green)
+        }
+        .frame(width: 140, height: 140)
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(.systemGray4), lineWidth: 2))
+        
+    }
+}
+
+struct TopMoversItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversItemView()
+    }
+}
---
+++ CryptoCurrency/Core/Home/Views/TopMoversView.swift
@@ -0,0 +1,32 @@
+//
+//  TopMoversView.swift
+//  CryptoCurrency
+//
+//  Created by Sonu Kumar on 03/07/23.
+//
+
+import SwiftUI
+
+struct TopMoversView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Top Currencies")
+                .font(.headline)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack (spacing: 16){
+                    ForEach(0 ..< 5, id: \.self){ _ in
+                        TopMoversItemView()
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct TopMoversView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversView()
+    }
+}
